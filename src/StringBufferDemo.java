f722e4e3b6abd8c5513672db3fde998be0e5ab46
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-04 13:54:29 -0500

    Java StringBuffer practice

---
+++ src/StringBufferDemo.java
@@ -0,0 +1,9 @@
+public class StringBufferDemo {
+    public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer("Edureka");
+        StringBuffer newString = s1.append("!!!");
+        System.out.println(newString);
+        System.out.println(newString.reverse());
+
+    }
+}
